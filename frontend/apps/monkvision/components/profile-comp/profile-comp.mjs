a21e635ed9e81e680b0f3576fde4ab7e126aa653
abhi-dlt <abhinav.verma@deeplogictech.com>
2024-02-23 05:06:37 +0000

    updated profile comp

--- frontend/apps/monkvision/components/profile-comp/profile-comp.mjs
+++ frontend/apps/monkvision/components/profile-comp/profile-comp.mjs
@@ -5,14 +5,42 @@
 import {router} from "/framework/js/router.mjs";
 import {APP_CONSTANTS} from "../../js/constants.mjs";
 
-var pageData = {}
-
-async function elementRendered(element) {
-    profile_comp.bindData(pageData)
+async function elementConnected(el) {
+    loadProfile(el);
 }
-function loadProfile(){
-    router.loadPage(APP_CONSTANTS.PROFILE_HTML)
+async function loadProfile(el, toggle=false){
+    let data = await getProfileData();
+    data.toggle = toggle;
+    try {
+        await profile_comp.bindData(data, el.id);
+    } catch (error) {
+        await profile_comp.bindData(data, el.id);
+    }
+}
+
+/**
+ * 
+ * Api call to get profile data
+ */
+async function getProfileData(){
+    return ({
+        name: "John",
+        dp: "./img/man1.jpg",
+        start: "",
+        end: ""
+    })
+}
+
+function toggle(c){
+    c = c.closest('#container');
+    // Toggle the position of the container div
+    if (c.style.right === '0px') {
+        c.style.right = '-30vw';
+    } else {
+        c.style.right = '0px';
+        c.closest('body').onclick = toggle;
+    }
 }
 
 const trueWebComponentMode = true;	// making this false renders the component without using Shadow DOM
-export const profile_comp = {trueWebComponentMode, trueJS:false, elementRendered,loadProfile}
+export const profile_comp = {trueWebComponentMode, trueJS:false, elementConnected,loadProfile}
