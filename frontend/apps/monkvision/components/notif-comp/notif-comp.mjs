c52786851a7f7c83b30cb1d1c6cf74cf5924a4a4
abhi-dlt <abhinav.verma@deeplogictech.com>
2024-03-04 11:43:27 +0000

    added notification component

--- frontend/apps/monkvision/components/notif-comp/notif-comp.mjs
+++ frontend/apps/monkvision/components/notif-comp/notif-comp.mjs
@@ -3,17 +3,39 @@
  * License: MIT - see enclosed license.txt file.
  */
 
-async function elementRendered(element) {
-    // const mainTitle = element.getAttribute('mainTitle');
-    // const data = element.getAttribute('data');
-    // const innerTitle = element.getAttribute('innerTitle');
-    // const innerSubtitle = element.getAttribute('innerSubtitle');
-    // const percentage = element.getAttribute('percentage');
-    // const color = element.getAttribute('color');
-    // const warningColour = element.getAttribute('warningcolour');
-    // element.shadowRoot.querySelector('')
-    console.log("working inside notif-comp")
+import { main } from "./../../js/main.mjs";
+
+async function elementConnected(el) {
+    configureElement(el);
+}
+
+async function configureElement(el){
+    try {
+        await notif_comp.bindData(el.dataset, el.id);
+    } catch (error) {
+        await notif_comp.bindData(el.dataset, el.id);
+    }
+}
+
+function scrollHandler(c) {
+    const totalChildren = c.children.length;
+    if(totalChildren<2) return setButtons(c, 0, 0);
+    const { scrollLeft, clientWidth } = c;
+    const currentChild = Math.floor(scrollLeft / clientWidth);
+    if (scrollLeft === 0) setButtons(c, 0, 1);
+    else if (currentChild === totalChildren - 1) setButtons(c, 1, 0) 
+    else setButtons(c, 1, 1);
+  }
+
+function scrollContent(el){
+    const carouselContainer = main.querySelector(el, '.content');
+    carouselContainer.scrollLeft += (el.id=='prev'? (-1) : 1)*carouselContainer.offsetWidth; // Scroll one slide width
+}
+
+function setButtons(el, ...vis){
+    const btns = ['#prev', '#next'].map(q => main.querySelector(el, q));
+    btns.forEach((btn, i) => btn.style.display = vis[i]? 'flex' : 'none'); 
 }
 
 const trueWebComponentMode = true;	// making this false renders the component without using Shadow DOM
-export const notif_comp = {trueWebComponentMode, trueJS:false, elementRendered}
+export const notif_comp = {trueWebComponentMode, trueJS:false, elementConnected, scrollHandler,scrollContent}
